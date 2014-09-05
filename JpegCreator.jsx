1285848e7910cbddc9f963ab14b2f9eed5278b78
Allandt Bik-Elliott <allandt@gmail.com>
2014-09-05 12:21:07 +0100

    add script and readme

---
+++ JpegCreator.jsx
@@ -0,0 +1,157 @@
+﻿/**
+    JpegCreator
+    creates jpg from a psd
+    @author     Allandt Bik-Elliott
+    @version    0.1b
+**/
+
+// file reference
+var docRef = app.activeDocument;
+preferences.rulerUnits = Units.PIXELS;
+
+// flatten and crop
+docRef.flatten();
+try  {
+    docRef.crop(docRef.selection.bounds);
+} catch (e) {}
+
+// constants
+var DEFAULT_FRAME_TIME  = 3;
+var END_FRAME_TIME = 5;
+var ACTION_FOLDER = "BannerActions";
+var DEFAULT_ACTION = "createAnimationFrame";
+var DEFAULT_PATH = "~/Desktop/";
+var DEFAULT_NAME = "output";
+var DEFAULT_FILESIZE = 50;
+var FILE_SIZE = parseFloat(docRef.width) + "x" + parseFloat(docRef.height);
+var END_ACTION = "createAnimationEndFrame";
+var QUALITY_VALUES = [99, 90, 80, 70, 60, 50, 40, 30, 20];
+var CACHE_FOLDER = Folder.myDocuments + '/photoshopScripts/';
+var CACHE_PATH = CACHE_FOLDER + 'savedsettingsjpgcreator.txt';
+
+// create folder if doesn't exist
+var cachefolder = new Folder(CACHE_FOLDER);
+if (!cachefolder.exists) cachefolder.create();
+
+// read in settings cache file
+var savedSettings = new File(CACHE_PATH);
+savedSettings.open ('r');
+var line = savedSettings.readln();
+
+// get values from csv
+var arSettings = line.split(',');
+savedSettings.close();
+
+// set names from defaults or cached values
+var startPath = '';
+var startName = '';
+var startSize = 0;
+
+if (arSettings.length > 1)
+{
+    startPath = arSettings[0];
+    startName = arSettings[1];
+    startSize = parseFloat(arSettings[2]);
+}
+else
+{
+    startPath = DEFAULT_PATH;
+    startName = DEFAULT_NAME;
+    startSize = DEFAULT_FILESIZE;
+}
+
+var numLayers = docRef.artLayers.length;
+var curLayer;
+
+// prompt user to set file path
+var targetPath = prompt ("save file path: ", startPath, "choose a save file path");
+if (!targetPath) targetPath = startPath;
+if (targetPath.charAt(targetPath.length - 1) !== "/") targetPath += "/";
+
+// check to see if director exists. If not, create it
+var targetFolder = new Folder(targetPath);
+if (!targetFolder.exists) targetFolder.create();
+
+// prompt user to set filename and generate filename-widthxheight
+var targetName =  prompt("save file name: ", startName, "choose a save file name") ;
+var targetFile = targetPath + targetName+ "-" + FILE_SIZE;
+if (!targetFile) targetFile = targetPath + startName + "-" + FILE_SIZE;
+
+// save psd
+$.writeln ("saving in " + targetFile);
+var saveOptions = new PhotoshopSaveOptions();
+saveOptions.layers = true;
+saveOptions.embedColorProfile = true;
+docRef.saveAs (new File(targetFile), saveOptions, false, Extension.LOWERCASE);
+
+// create gif
+// prompt user to set size
+var saveSize = prompt("target file size in kb: ", startSize, "target size for save");
+if (saveSize)
+{
+    saveSize *= 1024;
+}
+var options = new ExportOptionsSaveForWeb();
+options.format = SaveDocumentType.JPEG;
+
+//progress bar
+// thanks to http://www.ps-scripts.com/bb/viewtopic.php?t=786
+var value = 0; 
+var win = new Window("palette{text:'Please wait...',bounds:[100,100,550,140]," + 
+               "progress:Progressbar{bounds:[20,10,430,28] , minvalue:0,value:" + value + "}" +
+               "};"
+         );
+win.progress.maxvalue = QUALITY_VALUES.length;
+
+// loop through colour sizes array until filesize is under current size from array
+var sizeFound = false;
+var fileRef;
+for (var i = 0; i < QUALITY_VALUES.length; i++)
+{
+    // update progress bar
+    win.center(); 
+    win.show();
+    win.hide();
+    win.show();
+    win.progress.value++;
+    
+    // export doc as gif
+    fileRef = new File(targetFile + ".jpg");
+    docRef.exportDocument (fileRef, ExportType.SAVEFORWEB, options);
+    if (fileRef.length > saveSize)
+    {
+        // destroy old file
+        fileRef.remove();
+        
+        // set colors in options
+        options.quality = QUALITY_VALUES[i];
+        
+        $.writeln("attempting save with " + QUALITY_VALUES[i] + " quality");
+        
+        // create file
+        docRef.exportDocument (new File(targetFile + ".jpg"), ExportType.SAVEFORWEB, options);
+    }
+    else 
+    {
+        // file is already smaller than current value from QUALITY_VALUES so break the loop
+        $.writeln("filesize is " + fileRef.length);
+        sizeFound = true;
+        break;
+    }
+
+    $.writeln(targetFile + " save with size of " + Math.floor(fileRef.length / 1024) + "kb");
+}
+
+// if the size isn't found - make the user smile because they have failed miserably
+if (!sizeFound) alert("Couldn't get the jpg small enough - maybe you should rethink this");
+
+// cache settings
+savedSettings = new File(CACHE_PATH);
+savedSettings.open ('w');
+savedSettings.write ([targetPath, targetName,saveSize / 1024].toString());
+savedSettings.close();
+
+// save and close the psd
+docRef.save();
+docRef.close (SaveOptions.SAVECHANGES);
+
--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-photoshop-jpeg-creatorscript
+Photoshop Jpeg Creator script
 ============================
 
-works like the gif creator but for jpegs
+Photoshop script to create an optimised jpeg from a screengrab
+
+## How to install ##
+
+* Put the JpegCreator.jsx into `C:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Presets\Scripts` (Note: you may have a different version of photoshop so put it in the version you use)
+
+## Usage guide ##
+
+This will guide you through a series of steps to create the jpeg:
+
+* Select the area in the Photoshop file that the jpeg will be created from
+
+* You should run the action
+
+* Add the path to the directory you wish to save the files in
+
+![Add filepath](/img/grab-0.png)
+
+* Add the desired filename - this will have the size appended to it
+
+![Add filename](/img/grab-1.png)
+
+* Add the target filesize
+
+![Add filesize](/img/grab-3.png)
+
+This will store this information and will pre-populate the pop-up next time you use it which is useful for doing multiple jpegs in the same folder as you can use the same path and filename and the script will automatically add the size as a differentiator. 
+
+The script will then attempt to save a jpegs at the desired filesize by reducing the quality until the filesize is hit. If it can't do it (because the target filesize is not possible to hit at the dimensions given), the user will be given a cheerful message to that effect.
+
+
+## Useful Links ##
+* [Guide for installing Photoshop scripts](http://speedscraps.blogspot.co.uk/2010/04/installing-scripts-photoshop.html)
+* [Adobe's Photoshop Scripting Reference](http://www.adobe.com/devnet/photoshop/scripting.html)
+* [ps-scripts.com Forum](http://www.ps-scripts.com/bb/)
